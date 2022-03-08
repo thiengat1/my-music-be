85183987ef93d6d5182e82e27d0464e62967d817
thienga <thiengat1@gmail.com>
2022-03-08 23:57:23 +0800

    add env

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,14 +2,15 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2021-12-12 11:56:48
- * @LastEditTime: 2021-12-12 12:01:36
+ * @LastEditTime: 2022-03-08 23:15:19
  * @LastEditors: Lewis
  */
 
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/my_music_dev");
+    //await mongoose.connect("mongodb://localhost:27017/my_music_dev");
+    await mongoose.connect(process.env.DATABASE);
     console.log('connect successfully');
   } catch (err) {
     console.log('connect fail');
